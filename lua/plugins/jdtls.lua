3b5b11c54ff82c5842c7e760c075c13aa9b52429
Fuyang Liu <liufuyang@users.noreply.github.com>
2024-01-31 15:05:26 +0100

    wip

--- lua/plugins/jdtls.lua
+++ lua/plugins/jdtls.lua
@@ -98,7 +98,7 @@ return {
     opts = function(_, opts)
       local utils = require "astrocore"
       -- use this function notation to build some variables
-      local root_markers = { ".git", "mvnw", "gradlew", "pom.xml", "build.gradle", "build.gradle.kts", ".project" }
+      local root_markers = { ".git", "mvnw", "gradlew" }
       local root_dir = require("jdtls.setup").find_root(root_markers)
       -- calculate workspace dir
       local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
@@ -141,12 +141,45 @@ return {
         },
         root_dir = root_dir,
         settings = {
+          -- https://github.com/eclipse-jdtls/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
           java = {
             eclipse = { downloadSources = true },
             configuration = { updateBuildConfiguration = "interactive" },
             maven = { downloadSources = true },
             implementationsCodeLens = { enabled = true },
             referencesCodeLens = { enabled = true },
+            import = {
+              exclusions = {
+                "**/node_modules/**",
+                "**/.git/**",
+                ".git",
+                "**/.metadata/**",
+                "**/archetype-resources/**",
+                "**/META-INF/maven/**",
+                "**/build/**",
+                "**/bin/**",
+                "**/.project",
+                "**/.classpath",
+                ".project",
+                ".classpath",
+              }
+            },
+            project = {
+              resourceFilters = {
+                "**/node_modules/**",
+                "**/.git/**",
+                ".git",
+                "**/.metadata/**",
+                "**/archetype-resources/**",
+                "**/META-INF/maven/**",
+                "**/build/**",
+                "**/bin/**",
+                "**/.project",
+                "**/.classpath",
+                ".project",
+                ".classpath",
+              }
+            },
           },
           signatureHelp = { enabled = true },
           completion = {
